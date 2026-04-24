c9e28b3795b5f02679aea11e0e5330fb8dced2a2
Vikram Aruchamy <vikram@docstomarkdown.pro>
2026-04-24 18:58:14 +0530

    Updated documentation for claude

--- content/exporting-content/_meta.js
+++ content/exporting-content/_meta.js
@@ -1,5 +1,5 @@
 export default {
   "exporting-single-response": "Exporting Single response",
   "exporting-entire-conversation": "Exporting Entire conversation",
-  "exporting-canvas-content": "Exporting Canvas Content",
+  "exporting-artifact-content": "Exporting Artifact Content",
 };
